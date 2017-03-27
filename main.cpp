f6ae46317f2c9059446b068a4c7aea5dfabdb641
Mouli <sreekar.mouli1998@gmail.com>
2017-03-27 19:18:41 +0530

    Removes some excess code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 
 //  DEFINING PIECES
+#define EMPTY 0
 #define PAWN 1
 #define ROOK 2
 #define KNIGHT 3
@@ -16,19 +17,10 @@ int initializeBoard();
 
 //  DECLARING GLOBAL VARIABLES
 int whitePieces[6]={PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING};
-struct node {
-    int data;
-    struct node *next;
-};
-struct pos {
-    int x;
-    int y;
-};
+
 struct piece {
     int type;// LIKE PAWN, ROOK, BISHOP, etc
-    pos currentPos;
     int color;
-    int *possibleMoves;
 }board[8][8];//         ***MAY HAVE TO CHANGE THIS***
 
 //  MAIN METHOD
@@ -42,6 +34,11 @@ int main() {
 int initializeBoard() {//   INITIALIZES BOARD WITH PIECES ON THEIR DEFAULT POISITION
     int i,j;
     for(i=0; i<8; i++) {
+        for(j=0; j<8; j++) {
+            board[i][j].type = EMPTY;
+        }
+    }
+    for(i=0; i<8; i++) {
         board[1][i].type = board[6][i].type = PAWN;
     }
     board[0][0].type = board[0][7].type = board[7][0].type = board[7][7].type = ROOK;
@@ -49,12 +46,7 @@ int initializeBoard() {//   INITIALIZES BOARD WITH PIECES ON THEIR DEFAULT POISI
     board[0][2].type = board[0][5].type = board[7][2].type = board[7][5].type = BISHOP;
     board[0][3].type = board[7][3].type = QUEEN;
     board[0][4].type = board[7][4].type = KING;
-    for(i=0; i<8; i++) {
-        for(j=0; j<8; j++) {
-            board[i][j].currentPos.x = i;
-            board[i][j].currentPos.x = i;//         ***MAY HAVE TO CHANGE THIS***
-        }
-    }
+
     return 1;
 }
 
@@ -87,8 +79,8 @@ int printBoard() {//    DISPLAYS THE BOARD IN A SIMPLE WAY
                     printf("[K]");
                     break;
 
-                default:
-                    printf("   ");
+                case EMPTY:
+                    printf("---");
                     break;
 
             }
